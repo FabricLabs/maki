52cf82dff7677b95e416f3aa51db9fb6f86a420a
Eric Martindale <eric@ericmartindale.com>
2013-11-29 21:17:58 +0000

    Add examples to the header.

--- controllers/people.js
+++ controllers/people.js
@@ -1,6 +1,10 @@
 module.exports = {
   list: function(req, res, next) {
-    People.find().exec( res.provide );
+    People.find().exec(function(err, people) {
+      res.provide( err, people , {
+        template: 'people'
+      });
+    });
   },
   view: function(req, res, next) {
     People.findOne({ slug: req.param('usernameSlug') }).exec( res.provide );
