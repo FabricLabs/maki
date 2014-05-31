389150ac8a32584b6fbf6187df4ec8fe2fde5b01
Eric Martindale <eric@ericmartindale.com>
2014-05-31 03:39:21 -0400

    Add headroom.

--- public/js/maki.js
+++ public/js/maki.js
@@ -82,6 +82,26 @@ maki.controller('mainController', function( $scope ) {
       });
     }
   }
+}).directive('headroom', function() {
+  return {
+    restrict: 'EA',
+    scope: {
+      tolerance: '=',
+      offset: '=',
+      classes: '='
+    },
+    link: function(scope, element) {
+      var options = {};
+      angular.forEach(Headroom.options, function(value, key) {
+        options[key] = scope[key] || Headroom.options[key];
+      });
+      var headroom = new Headroom(element[0], options);
+      headroom.init();
+      scope.$on('destroy', function() {
+        headroom.destroy();
+      });
+    }
+  };
 });
 maki.controller('headerController', function( $scope , $location ) {
   $scope.isActive = function (viewLocation) {
