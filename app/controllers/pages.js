6539758083bdf4ddcd5f5faa3bc4d5fbaf6bc6fc
Eric Martindale <eric@ericmartindale.com>
2014-07-15 19:33:40 +0000

    Bind index controller correctly.

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -1,10 +1,7 @@
-var endpoints = {
-  register: function( endpoint ) {
-    
-  }
-}
-
 module.exports = {
+  index: function(req, res, next) {
+    res.provide('index');
+  },
   examples: function(req, res, next) {
     require('fs').readFile('data/examples.json', function(err, data) {
       res.provide( 'examples', {
