fa98f6689c7816d7590128acad2c2cfbe3dc39d0
Eric Martindale <eric@ericmartindale.com>
2014-08-22 16:23:10 -0400

    Reasonable Resource definition.

--- maki.js
+++ maki.js
@@ -3,4 +3,19 @@ var config = require('./config');
 var Maki = require('./lib/Maki');
 var maki = new Maki( config );
 
+var resources = [
+  {
+    name: 'Page',
+    attributes: {
+      name: { type: String , max: 80 },
+      slug: { type: String , max: 80 , id: true },
+      content: { type: String }
+    }
+  }
+];
+
+resources.forEach(function(resource) {
+  maki.define( resource.name , resource );
+});
+
 maki.start();
