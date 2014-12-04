17d25c92155f50ab342bead68e39e3082e614e25
Eric Martindale <eric@ericmartindale.com>
2014-12-03 23:19:26 -0500

    Eliminate extraneous resources.

--- maki.js
+++ maki.js
@@ -3,20 +3,6 @@ var config = require('./config');
 var Maki = require('./lib/Maki');
 var maki = new Maki( config );
 
-maki.define('Person', {
-  attributes: {
-    username: { type: String , max: 80 , required: true , slug: true },
-    hash:     { type: String , restricted: true },
-    salt:     { type: String , restricted: true },
-    email:    { type: String , max: 80 , restricted: true },
-    created:  { type: Date , default: Date.now, restricted: true }
-  },
-  plugins: [
-    require('passport-local-mongoose')
-  ],
-  icon: 'user'
-});
-
 maki.define('Example', {
   attributes: {
     name:    { type: String , max: 80 },
@@ -24,29 +10,6 @@ maki.define('Example', {
     content: { type: String }
   },
   icon: 'idea'
-});
-
-maki.define('Dashboard', {
-  attributes: {
-      name: { type: String , max: 80 , name: true , slug: true , required: true }
-    , _people: { type: require('mongoose').SchemaTypes.ObjectId, ref: 'Person' }
-  },
-  requires: {
-    'examples': {},
-    'people': {
-      filter: {
-        // _id: { $in: dashboard._people }
-      }
-    }
-  },
-  icon: 'dashboard'
-});
-
-maki.define('Widget', {
-  attributes: {
-    name: { type: String , max: 80 }
-  },
-  icon: 'setting'
 });
 
 maki.serve(['http']).start();
