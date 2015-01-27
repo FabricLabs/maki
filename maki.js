4c48ac3c6c35330fbdde2e85e51491dab2aa6ca0
Eric Martindale <eric@ericmartindale.com>
2015-01-26 22:24:59 -0500

    Add support for mappers and renderers.

--- maki.js
+++ maki.js
@@ -22,10 +22,30 @@ maki.define('Example', {
     name:    { type: String , max: 80 },
     slug:    { type: String , max: 80 , id: true },
     content: { type: String },
-    screenshot: { type: Maki.types.File }
+    screenshot: { type: 'File' }
   },
   source: 'data/examples.json',
   icon: 'idea'
+});
+
+maki.define('Release', {
+  attributes: {
+    name: { type: String , max: 80 },
+    tag: { type: String , max: 80 },
+    created: { type: Date },
+    published: { type: Date },
+    notes: { type: String , render: 'markdown' }
+  },
+  source: 'https://api.github.com/repos/martindale/maki/releases',
+  icon: 'tags',
+  map: function( release ) {
+    return {
+      name: release.name,
+      tag: release.tag_name,
+      notes: release.body,
+      published: new Date( release.published_at )
+    };
+  }
 });
 
 maki.define('Dashboard', {
