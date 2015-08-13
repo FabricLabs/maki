76b5488e971c7d8fdfa6cf3b927740b94882b754
Eric Martindale <eric@ericmartindale.com>
2015-08-13 14:31:03 -0700

    Change wording and branding to reflect broader vision.

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
    meaningful values to the rest of the application.
    This is done using the module.exports function,
    which sets them when require('./thisfile') is run. */
-   
+
 var NAMESPACE = process.env.MAKI_SERVICE_NAMESPACE || 'maki';
 var DATABASE  = process.env.MAKI_DATABASE_NAME || NAMESPACE;
 
@@ -11,24 +11,32 @@ module.exports = {
     name: process.env.MAKI_SERVICE_NAME || 'Maki' ,
     authority: process.env.MAKI_SERVICE_AUTHORITY || 'localhost:9200' ,
     namespace: NAMESPACE ,
-    mission: process.env.MAKI_SERVICE_MISSION || 'A simple framework for hand-rolling your web application.',
-    description: process.env.MAKI_SERVICE_DESCRIPTION || 'Maki is an "architecture as a library" project, allowing you to focus on building your project rather than laboring over architecture decisions.',
+    mission: process.env.MAKI_SERVICE_MISSION || 'A simple framework for quickly building apps.',
+    description: process.env.MAKI_SERVICE_DESCRIPTION || 'Maki allows you to focus on your project, rather than laboring over architecture decisions.  With true isomorphism, you control how your application behaves, and can then customize how your features are exposed on specific platforms.',
     source: 'https://github.com/martindale/maki',
     points: [
       {
-        header: 'Hand-rolled.',
-        description: 'No black boxes or arcane scribbling here.  Pure Javascript, and direct access to everything.'
-      },
-      {
-        header: 'Get Making.',
-        description: 'No black boxes or arcane scribbling here.  Pure Javascript, and direct access to everything.'
-      },
-      {
-        header: 'Already registered?',
-        description: 'Go on then.  Get logged in.  You\'re _groovy_.',
+        header: 'Build Once, Deploy Everywhere',
+        description: 'Maki can build desktop and <strong>native</strong> mobile apps directly from your web app.  All with the same code.',
         action: {
-          text: 'Log In &raquo;',
-          link: '/sessions'
+          text: 'Read the Docs &raquo;',
+          link: '/docs'
+        }
+      },
+      {
+        header: 'Self-Documenting',
+        description: 'Once you\'ve defined your resources, the API builds itself!  It works the same way everywhere.',
+        action: {
+          text: 'See the API &raquo;',
+          link: '/api'
+        }
+      },
+      {
+        header: 'Plugin Ecosystem',
+        description: 'There\'s already <em>tons</em> of plugins for Maki that implement common application functionality.',
+        action: {
+          text: 'Browse the Plugins &raquo;',
+          link: '/plugins'
         }
       }
     ],
