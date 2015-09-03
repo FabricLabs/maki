1fcc3f9e784191506e304ce3ac88bb09c4d9f8c2
Eric Martindale <eric@ericmartindale.com>
2015-09-03 12:45:41 -0700

    Begin to add a CLI tool for rolling apps.

--- bin/maki.js
+++ bin/maki.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
-
-var Maki = require('../lib');
+var Maki = require('../lib/Maki');
 var program = require('commander');
 
 program
-  .parse( process.argv );
+  .version(require('../package.json').version)
+  .command('maki <command> [parameters...]')
+  .option('define <name>', 'Define a resource.')
+  .on('define', function() {
+    console.log('heyyyy!');
+  });
+
+
+
+
+program.parse(process.argv);
